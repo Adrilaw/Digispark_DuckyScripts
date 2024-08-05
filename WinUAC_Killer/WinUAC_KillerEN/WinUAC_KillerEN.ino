758277b7aac3cc71e02723abcf52a649ba488809
Mel-Adrien Dodin <65346144+Adrilaw@users.noreply.github.com>
2024-08-05 15:50:02 +0400

    Update WinUAC_KillerEN.ino

--- WinUAC_Killer/WinUAC_KillerEN/WinUAC_KillerEN.ino
+++ WinUAC_Killer/WinUAC_KillerEN/WinUAC_KillerEN.ino
@@ -9,6 +9,13 @@ void loop() {}
 void setup() {
   DigiKeyboard.sendKeyStroke(0);
   DigiKeyboard.delay(1000);
+  // Title: Disable Windows Defender
+  // Author: Zero_Sploit (v1.0)
+  // Updated: B33m0 (v1.1)
+  // Updated: UberGuidoZ (v1.2)
+  // Description: Opens security settings, disabled Defender, then adds an exception of drive C for persistence
+  // Target: Windows 10/11 (Powershell)
+  // Version: 1.2
   // Description: Opens security settings, disabled Defender, then adds an exception of drive C for persistence
   // Target: Windows 10/11 (Powershell)
   // Version: 1.2
